88c9c86e1fc2145927d459d142ca6370c774a7f9
Luis Velasquez <mv.prgithub@gmail.com>
2023-11-02 01:56:56 -0600

    cors

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ app.use(
     origin: "http://localhost:3000", // Reemplaza con tu dominio
   })
 );
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  next();
+});
 app.use(express.json());
 
 // Usa las rutas
