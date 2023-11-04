4100cfb0e5a95e1410200b09a1774d99114cc2ec
Luis Velasquez <139912145+Marianovh@users.noreply.github.com>
2023-11-03 21:24:46 -0600

    h

--- app.js
+++ app.js
@@ -14,17 +14,31 @@ const cors = require("cors"); // Importa el paquete 'cors'
 connect();
 // Configura el middleware de CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // Es lo mismo que la configuracion de CORS de abajo
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://netlify--coruscating-queijadas-1247a0.netlify.app"
+  ); // Es lo mismo que la configuracion de CORS de abajo
+  // res.header("Access-Control-Allow-Origin", "https://l5kbp6rc-3000.use2.devtunnels.ms"); // Es lo mismo que la configuracion de CORS de abajo
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
-// app.use(
-//   cors({
-//     origin: "http://localhost:3001",
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    origin: "https://netlify--coruscating-queijadas-1247a0.netlify.app", // Reemplaza con tu dominio
+  })
+);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  next();
+});
 
 app.use(express.json());
 
